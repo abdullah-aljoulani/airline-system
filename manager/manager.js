916ebde802535635724fe560743d8296ddca3d06
abdullah-aljoulani <abdullahaljoulani@gmail.com>
2023-07-12 20:42:52 +0000

    fixing the files

--- manager/manager.js
+++ manager/manager.js
@@ -4,41 +4,19 @@ require('dotenv').config()
 
 const port = process.env.PORT || 3000
 
-// require the client socket
-const server = require('socket.io-client');
+const server = require('socket.io-client')
 
-// connect the socket to the server
-const socket = server(`http://localhost:${port}`);
+const host = `http://localhost:${port}`
 
-// import faker to get random data
-const { faker } = require('@faker-js/faker');
+const socket = server(host)
 
-// event the flight to get the data from the new flight
-socket.on('flight', flighthandler)
-
-// set time to loop through to get random data every 10 sec
-setInterval(() => {
-    let data = {
-        details: {
-            id: faker.string.uuid(),
-            pilotes: faker.internet.userName(),
-            destinations: faker.location.country(),
-            airline: faker.definitions.airline.airline[0].name,
-        }
-    }
-    // emit the faker data to the new flight event
-    socket.emit('new-flight', data)
-}, 10000)
-
-
-// handle the flight event hanlder which we already give it the data from the new flight
-function flighthandler(payload) {
-    console.log(`Manager: new flight with ID: ${payload.details.id} have been scheduled:`, new Date())
-    payload.event = 'new-flight'
-    payload.data = new Date()
-    socket.emit('logStatus', payload)
-}
-
-socket.on('notifyManager', payload => {
-    console.log(`Manager: we’re greatly thankful for the amazing flight, ${payload.details.pilotes}`);
+socket.on('new-flight', data => {
+    console.log(` Manager: new flight with ID ${data.Details.flightID} have been scheduled`);
+    let scheduled = {
+        airLine: data.Details.airLine,
+        flightID: data.Details.pilot,
+        pilot: data.Details.flightID,
+        destination: data.Details.destination
+    };
+    socket.emit('managerAlert', scheduled)
 })
